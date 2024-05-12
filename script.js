481f7df4d000c39a6204581e29f349e336887982
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-05-12 13:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+	return emailRegex.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
-alert(validEmail(str));
+alert(validEmail(str)); 
+   
